16f2aca98ab76d810ccfc10290afb2c8dc6f3e95
D-lore-unipd <root@LaptopDavide.localdomain>
2025-08-24 15:16:19 +0200

    LayeredRenderSystem completed

--- AudioVisualizer/cmake/extra.cmake
+++ AudioVisualizer/cmake/extra.cmake
@@ -10,4 +10,25 @@
 
 ### INIZIO AREA AUTOMATICA ###
 target_sources(${PROJECT_NAME} PRIVATE
+    src/AudioEngine.cpp
+    src/AudioEngine.h
+    src/DesignPattern/CircularVisualization.cpp
+    src/DesignPattern/CircularVisualization.h
+    src/DesignPattern/FrequencyBarsVisualization.cpp
+    src/DesignPattern/FrequencyBarsVisualization.h
+    src/DesignPattern/IVisualizationStrategy.cpp
+    src/DesignPattern/IVisualizationStrategy.h
+    src/DesignPattern/ShaderVisualization.cpp
+    src/DesignPattern/ShaderVisualization.h
+    src/DesignPattern/Utility.h
+    src/DesignPattern/WaveformVisualization.cpp
+    src/DesignPattern/WaveformVisualization.h
+    src/FFTAnalyzer.cpp
+    src/FFTAnalyzer.h
+    src/MainWindow.cpp
+    src/MainWindow.h
+    src/TestFFTAnalyzer.cpp
+    src/Visualizer.cpp
+    src/Visualizer.h
+    src/main.cpp
 )
